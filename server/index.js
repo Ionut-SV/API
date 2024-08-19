dd0e7684eff1a6f7347af236dedff69dac780b96
Ionut-SV <volintiru.ionut@yahoo.com>
2024-08-19 11:50:03 +0300

    backend update

--- server/index.js
+++ server/index.js
@@ -1,34 +1,44 @@
+require('dotenv').config();
+
+
 const express = require ('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRouter = require('./routes/authRoute.js')
 const fileRouter = require('./routes/fileRoute.js');
 const userRouter = require('./routes/userRoute.js');
-const commentRoutes = require('./routes/commentsRoute.js');
 const cookieParser = require('cookie-parser');
+const commentsRouter = require('./routes/commentsRoute.js');
+
 
 const app = express();
+// Adaugă această linie înainte de `mongoose.connect()`
+const MongoDB_URI = process.env.MONGODB_URI
+console.log('MongoDB URI:', process.env.MONGODB_URI); 
+ // Verifică toate variabilele de mediu
 
  //MiddleWares
  app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
     credentials: true,
 }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/uploads', express.static('uploads'));
 
  //Routes
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+ app.use('/api/user', userRouter);
+app.use ('/api/auth', authRouter);
 app.use('/api/files', fileRouter);
-app.use('/api/comments', commentRoutes);
+app.use('/api/comments',commentsRouter);
 
 
  //MongoDB Connection
 mongoose
-    .connect('mongodb+srv://telacad:telacad2024@telacad.ms1pwzj.mongodb.net/?retryWrites=true&w=majority&appName=Telacad')
+    .connect(MongoDB_URI)
     .then(() => console.log('Connected to MongoDB!'))
     .catch((error)=>console.error("Failed to connect to MongoDB:", error));
 
@@ -44,7 +54,7 @@ app.use((err, req, res, next) =>{
 });
 
  //Server
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
 });
